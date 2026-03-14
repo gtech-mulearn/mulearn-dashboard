2566a7dcb1f44b0315d423a2d8a34eda758abf73
Akshayvs-Tech <kichi4295@gmail.com>
2026-03-14 15:37:38 +0530

    endpoint changed from  dashboard/discord-moderation to dashboard/management/discord-moderation

--- src/app/(dashboard)/dashboard/discord-moderation/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DiscordModerationPage } from "@/features/discord-moderation";
-
-export const metadata = {
-  title: "Discord Moderation",
-};
-
-export default function Page() {
-  return <DiscordModerationPage />;
-}
