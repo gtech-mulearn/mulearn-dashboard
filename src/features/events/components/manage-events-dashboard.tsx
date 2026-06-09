a1d82b58c5857c36fc3128b56eef3dfd9520ecae
Devika <dileepdevika3@gmail.com>
2026-06-09 13:45:10 +0530

    refactor: remove unused utility function import in ManageEventsDashboard

--- src/features/events/components/manage-events-dashboard.tsx
+++ src/features/events/components/manage-events-dashboard.tsx
@@ -10,7 +10,6 @@ import { Input } from "@/components/ui/input";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useUserInfo } from "@/features/auth/hooks";
 import { ROLES } from "@/lib/auth/roles";
-import { cn } from "@/lib/utils";
 import { eventsApi } from "../api";
 import { MANAGE_EVENT_STATUS_PILLS } from "../constants/events.constants";
 import { usePendingCollaboratorInvites } from "../hooks";
